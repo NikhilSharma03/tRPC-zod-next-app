fdd69b17e03bca324c6a5e1ac1abe0c98801d283
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2022-10-15 03:33:35 +0530

    implemented scroll to bottom

--- src/components/ChatContainer/ChatContainer.tsx
+++ src/components/ChatContainer/ChatContainer.tsx
@@ -2,14 +2,27 @@ import { NextPage } from "next";
 import MessageItem from "../MessageItem/MessageItem";
 import { Message } from "../../types/message";
 import { motion, AnimatePresence } from "framer-motion";
+import { useEffect, useRef } from "react";
 
 type Props = {
   msgs: Message[];
 };
 
 const ChatContainer: NextPage<Props> = ({ msgs }) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      const scroll = ref.current.scrollHeight - ref.current.clientHeight;
+      ref.current.scrollTo(0, scroll);
+    }
+  }, [msgs]);
+
   return (
-    <div className="overflow-y-auto overflow-x-hidden p-4 h-[63vh] chatbox-container flex flex-col">
+    <div
+      className="overflow-y-auto overflow-x-hidden p-4 h-[63vh] chatbox-container flex flex-col"
+      ref={ref}
+    >
       <div className="ghost-element"></div>
       <AnimatePresence initial={false}>
         {msgs.map((msg) => (
